5a6324484923da62c80fcbf27177db6c9f5f0869
unosega@gmail.com <unosega@gmail.com>
2026-05-23 12:48:27 +0700

    first commit

---
+++ final-exam-wads/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Hello World - Final Exam WADS</h1>
+      <Button variant="outline" className="text-red-600">Click me</Button>
+    </div>
+  );
+}
---
+++ quiz-wads/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World - Quiz WADS</h1>
+      <Button variant="destructive" className="text-red-600">Click me</Button>
+    </div>
+  );
+}
